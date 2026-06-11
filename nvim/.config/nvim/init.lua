ba6ed3c9f5d6c3a7a9cf0f242145d6987b0e4c5e
Nitin Dixit <nitinajaydixit@gmail.com>
2026-06-11 20:55:41 +0530

    fix: remove matugen theming for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -43,8 +43,3 @@ hi LineNr guifg=#6c7086
   highlight Winbar guibg=none
 ]]
 
-vim.cmd("colorscheme matugen")
-vim.api.nvim_create_autocmd("Signal", {
-  pattern = "SIGUSR1",
-  command = "colorscheme matugen",
-})
