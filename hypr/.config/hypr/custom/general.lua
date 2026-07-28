5464021b5dc82c86d723fa35b133cae246d53618
Nitin Dixit <nitinajaydixit@gmail.com>
2026-07-28 19:34:43 +0530

    chore: update hypr animations, blur , zathura bg recolor

--- hypr/.config/hypr/custom/general.lua
+++ hypr/.config/hypr/custom/general.lua
@@ -10,12 +10,12 @@ hl.config({
 	},
 	decoration = {
 		blur = {
-			enabled = false,
-			size = 3,
-			passes = 4,
+			enabled = true,
+			size = 2,
+			passes = 1,
 			new_optimizations = true,
 			ignore_opacity = true,
-			xray = true,
+			xray = false,
 		},
 		shadow = {
 			enabled = false,
@@ -23,6 +23,58 @@ hl.config({
 		dim_inactive = false,
 	},
 	animations = {
-		enabled = false,
+		enabled = true,
 	},
 })
+
+local prod = function(ds)
+	return ds * 0.2
+end
+
+hl.curve("macOpen", { type = "spring", mass = 1, stiffness = 110, dampening = 16 })
+hl.curve("macBounce", { type = "spring", mass = 1, stiffness = 80, dampening = 10 })
+hl.curve("macSmooth", { type = "bezier", points = { { 0.25, 0.1 }, { 0.25, 1.0 } } })
+hl.animation({ leaf = "fadeIn", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeOut", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeSwitch", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeShadow", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeDim", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeLayers", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadePopups", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadePopupsIn", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadePopupsOut", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "fadeDpms", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "windows", enabled = true, speed = prod(8), spring = "macOpen", style = "popin 90%" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = prod(8), spring = "macOpen", style = "popin 90%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = prod(7), spring = "macBounce", style = "popin 90%" })
+hl.animation({ leaf = "windowsMove", enabled = true, speed = prod(5), bezier = "macSmooth", style = "slide" })
+hl.animation({ leaf = "border", enabled = true, speed = prod(1), bezier = "default" })
+hl.animation({ leaf = "fade", enabled = true, speed = prod(8), bezier = "default" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = prod(6), bezier = "macSmooth", style = "slidefade 20%" })
+hl.animation({ leaf = "workspacesIn", enabled = true, speed = prod(6), bezier = "macSmooth", style = "slidefade 20%" })
+hl.animation({ leaf = "workspacesOut", enabled = true, speed = prod(6), bezier = "macSmooth", style = "slidefade 20%" })
+hl.animation({
+	leaf = "specialWorkspace",
+	enabled = true,
+	speed = prod(6),
+	bezier = "macSmooth",
+	style = "slidefade 20%",
+})
+hl.animation({
+	leaf = "specialWorkspaceIn",
+	enabled = true,
+	speed = prod(6),
+	bezier = "macSmooth",
+	style = "slidefade 20%",
+})
+hl.animation({
+	leaf = "specialWorkspaceOut",
+	enabled = true,
+	speed = prod(6),
+	bezier = "macSmooth",
+	style = "slidefade 20%",
+})
+hl.animation({ leaf = "zoomFactor", enabled = true, speed = prod(7), bezier = "default" })
+hl.animation({ leaf = "monitorAdded", enabled = true, speed = prod(7), bezier = "default" })
--- hypr/.config/hypr/custom/rules.lua
+++ hypr/.config/hypr/custom/rules.lua
@@ -7,29 +7,48 @@
 hl.window_rule({
 	match = { class = "kitty" },
 	opacity = 0.9,
+	no_blur = false,
 })
 
 hl.window_rule({
-	match = { class = "QMPlay2" },
+	match = { class = "com.mitchellh.ghostty" },
+	no_blur = false,
 	opacity = 0.9,
 })
 
 hl.window_rule({
-	match = { class = "superproductivity" },
-	opacity = 0.93,
+	match = { class = "superproductivity-bin" },
+	opacity = 0.9,
+	monitor = "eDP-1",
+	no_blur = false,
 })
 
+hl.window_rule({
+	match = { class = "obsidian" },
+	opacity = 0.9,
+	no_blur = false,
+})
 hl.window_rule({
 	match = { class = "org.pwmt.zathura" },
 	opacity = 0.9,
+	monitor = "eDP-1",
+	no_blur = false,
 })
 
 hl.window_rule({
 	match = { class = "org.gnome.SystemMonitor" },
-	opacity = 0.91,
+	opacity = 0.9,
+	no_blur = false,
 })
 
 hl.window_rule({
 	match = { class = "equibop" },
-	opacity = 0.91,
+	opacity = 0.9,
+	no_blur = false,
+})
+
+hl.window_rule({
+	match = { class = "org.kde.dolphin" },
+	opacity = 0.9,
+	no_blur = false,
 })
