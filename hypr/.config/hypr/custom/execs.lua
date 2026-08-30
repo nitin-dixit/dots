2b1783f902c922a2dbf10ceeb5e419c16ea2e7cf
Nitin Dixit <nitinajaydixit@gmail.com>
2026-08-30 09:59:41 +0530

    fix(starship): add command timeout

--- hypr/.config/hypr/custom/execs.lua
+++ hypr/.config/hypr/custom/execs.lua
@@ -1,6 +1,6 @@
 hl.on("hyprland.start", function()
 	-- Bar, wallpaper
-	hl.exec_cmd("systemctl --user enable --now hyprvoice.service")
+	hl.exec_cmd("otd-daemon")
 	hl.exec_cmd("hyprctl setcursor Bibata-Modern-Ice 18")
 	hl.exec_cmd("kdeconnect-indicator")
 	hl.exec_cmd("~/Developer/nitin-dixit/configs/dots/end-4-hyprland/custom/scripts/handle_monitor_connect.sh")
